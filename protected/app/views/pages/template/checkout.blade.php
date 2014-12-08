0462119ddeb1409fdd1341d08abd682773f1bacf
TranQuocDung <tran.dung@mulodo.com>
2014-12-08 17:24:49 +0700

    asdasd

--- protected/app/views/pages/template/checkout.blade.php
+++ protected/app/views/pages/template/checkout.blade.php
@@ -1,4 +1,47 @@
-<div class="checkout">
+    <div id="content">
+      <!--Breadcrumb Part Start-->
+      <div class="breadcrumb"> <a href="index-2.html">Home</a> » <a href="#">Shopping Cart</a></div>
+      <!--Breadcrumb Part End-->
+      <h1>Checkout</h1>
+      <div class="checkout">
+        <div class="checkout-heading">Step 1: Checkout Options</div>
+        <div class="checkout-content" style="display: block;">
+          <div class="left">
+            <h2>New Customer</h2>
+            <p>Checkout Options:</p>
+            <label for="register">
+              <input type="radio" checked="checked" id="register" value="register" name="account">
+              <b>Register Account</b></label>
+            <br>
+            <label for="guest">
+              <input type="radio" id="guest" value="guest" name="account">
+              <b>Guest Checkout</b></label>
+            <br>
+            <br>
+            <p>By creating an account you will be able to shop faster, be up to date on an order's status, and keep track of the orders you have previously made.</p>
+            <input type="button" class="button" id="button-account" value="Continue">
+            <br>
+            <br>
+          </div>
+          <div class="right" id="login">
+            <h2>Returning Customer</h2>
+            <p>I am a returning customer</p>
+            <b>E-Mail:</b><br>
+            <input type="text" value="" name="email">
+            <br>
+            <br>
+            <b>Password:</b><br>
+            <input type="password" value="" name="password">
+            <br>
+            <a href="#">Forgotten Password</a><br>
+            <br>
+            <input type="button" class="button" id="button-login" value="Login">
+            <br>
+            <br>
+          </div>
+        </div>
+      </div>
+      <div class="checkout">
         <div class="checkout-heading">Step 6: Confirm Order</div>
         <div class="checkout-content">
           <div class="checkout-product">
@@ -15,8 +58,8 @@
               <tbody>
                 @foreach($cart as $item)
                   <tr>
-                    <td class="name"><a href="{{$item->link}}">{{$item['ProductName']}}</a></td>
-                    <td class="model">{{$item['CategoryName']}}</td>
+                    <td class="name"><a href="{{$item['link']}}">{{$item['ProductName']}}</a></td>
+                    <td class="model">{{$item['categoryname']}}</td>
                     <td class="quantity">{{$item['sl']}}</td>
                     <td class="price">{{$item['price']}} {{$item['price_promition']}}</td>
                     <td class="total">{{$item['price_total']}}</td>
@@ -25,16 +68,8 @@
               </tbody>
               <tfoot>
                 <tr>
-                  <td class="price" colspan="4"><b>Sub-Total:</b></td>
-                  <td class="total">£51.11</td>
-                </tr>
-                <tr>
-                  <td class="price" colspan="4"><b>Flat Shipping Rate:</b></td>
-                  <td class="total">£3.19</td>
-                </tr>
-                <tr>
                   <td class="price" colspan="4"><b>Total:</b></td>
-                  <td class="total">£66.37</td>
+                  <td class="total">{{$total_real}}</td>
                 </tr>
               </tfoot>
             </table>
@@ -46,3 +81,4 @@
           </div>
         </div>
       </div>
+    </div>
